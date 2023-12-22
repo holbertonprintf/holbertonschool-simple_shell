617c67e60f0fd7d990e5a09917e08bd188330816
vctrams <vctrams@vctrams@gmail.com>
2023-12-22 13:49:41 -0800

    prototypes added

--- shell.h
+++ shell.h
@@ -13,5 +13,6 @@ int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
 int word_count(char *str, char *delim);
 char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 
 #endif
