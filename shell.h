11a2436989c562d0016a470655ffd5db47f405a0
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2023-12-22 12:46:52 -0800

    added function of strtok that tokenize a string

--- shell.h
+++ shell.h
@@ -7,5 +7,7 @@
 int _getchar(void);
 ssize_t else_handle_input(char *lineptr, int stream, char *input, int filled);
 ssize_t _getline(char *lineptr, int stream);
+void _free(char **list, int count);
+char **_strtok(char *str, char *delim);
 
 #endif
