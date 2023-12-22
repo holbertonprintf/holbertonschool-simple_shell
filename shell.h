b2b3a268c5f0a4385bbf437b8324fa0ef5aefe34
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2023-12-22 10:14:26 -0800

    added main header and function of getline and getchar

---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stddef.h>
+#include <unistd.h>
+
+int _getchar(void);
+ssize_t else_handle_input(char *lineptr, int stream, char *input, int filled);
+ssize_t _getline(char *lineptr, int stream);
+
+#endif
